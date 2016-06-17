763d080861e1ee4e85f5cdbba1b727240b7a8bf6
himanshusaini-91 <himanshusaini0055@gmail.com>
2016-06-17 18:37:35 +0530

    calc

---
+++ calculator/model/model.js
@@ -0,0 +1,20 @@
+var obj={
+
+    add:function(x,y)
+    {
+        return x+y;
+    },
+    subs:function(x,y)
+    {
+        return x-y;
+    },
+    mul:function(x,y)
+    {
+        return x*y;
+    },
+    div:function(x,y)
+    {
+        return x/y;
+    }
+
+};
